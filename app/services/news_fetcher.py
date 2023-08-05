194f741936326a3bae32e7796cdb56fb746931ba
Srisha <srisha.balaji7@gmail.com>
2023-08-05 15:38:17 -0500

    newsfetcher API update

--- app/services/news_fetcher.py
+++ app/services/news_fetcher.py
@@ -1,9 +1,9 @@
-import requests
-import os
-from typing import List
-from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from pathlib import Path
+import httpx
+from datetime import datetime, timedelta
+from typing import List, Dict
+import os
 
 dotenv_path = Path('.env')
 # print(dotenv_path.resolve())
@@ -11,33 +11,39 @@ dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 class NewsFetcher:
-    def __init__(self):
-        self.API_KEY = os.getenv("NEWSAPI_API_KEY")
-        self.BASE_URL = "https://newsapi.org/v2/"
+    BASE_URL = "https://api.newscatcherapi.com/v2/search"
+    HEADERS = {
+        "x-api-key": os.getenv("NEWSFETCHER_API_KEY")
+    }
 
-    def fetch_news(self, query: str, days_ago: int = 1, sort_by: str = 'popularity', page_size: int = 10) -> List[dict]:
-        """
-        Fetch news articles from NewsAPI based on a query, from a certain number of days ago.
-        :param query: The topic of interest to fetch news about
-        :param days_ago: The number of days ago from which to fetch news
-        :param sort_by: How to sort the fetched news. Default is 'popularity'
-        :param page_size: The number of articles to return per request. Default is 10
-        :return: A list of news articles, each represented as a dictionary
-        """
-        from_date = (datetime.now() - timedelta(days=days_ago)).date().isoformat()
-        url = f"{self.BASE_URL}everything?q={query}&from={from_date}&sortBy={sort_by}&pageSize={page_size}&apiKey={self.API_KEY}"
-
-        response = requests.get(url)
+    @classmethod
+    def fetch_latest_news(cls, keywords: List[str], lang: str = "en", page: int = 3) -> List[Dict]:
+        # Calculate date range for today and yesterday
+        today = datetime.today().strftime('%Y-%m-%d')
+        yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+        
+        # Combine keywords using 'AND' operator
+        query = " AND ".join(keywords)
+        
+        params = {
+            "q": query,
+            "lang": lang,
+            "from": yesterday,
+            "to": today,
+            "page": page
+        }
+        
+        response = httpx.get(cls.BASE_URL, headers=cls.HEADERS, params=params)
         data = response.json()
+        return data.get("articles", [])
 
-        if data['status'] != 'ok':
-            print(f"Error fetching news: {data.get('message', 'No message provided')}")
-            return []
-
-        return data['articles']
-
-# Test the class
 if __name__ == "__main__":
-    fetcher = NewsFetcher()
-    articles = fetcher.fetch_news('technology')
-    print(articles)
+    keywords = ["technology","china","environment"]
+    articles = NewsFetcher.fetch_latest_news(keywords=keywords)
+    for article in articles:
+        print("Title:", article["title"])
+        print("Author:", article["author"])
+        print("Published Date:", article["published_date"])
+        print("Summary:", article["summary"])
+        print("URL:", article["link"])
+        print("-------------------------------------------------")
