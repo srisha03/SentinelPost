31593fc5c9459fadc541b71280e6731311af1404
Srisha <srisha.balaji7@gmail.com>
2023-08-12 18:52:17 -0500

    stable diffusion img gen

---
+++ app/services/img_gen.py
@@ -0,0 +1,18 @@
+import torch
+# from diffusers import StableDiffusionPipeline
+
+# model_id = "CompVis/stable-diffusion-v1-4"
+# device = "cuda"
+
+# pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+# pipe = pipe.to(device)
+
+# prompt = "a photo of an astronaut riding a horse on mars"
+# image = pipe(prompt).images[0]  
+    
+# image.save("astronaut_rides_horse.png")
+
+import torch
+
+print(torch.version.cuda)
+print(torch.cuda.is_available())
